7f52e47995bdd5410730feb5e6a31f233b297b7a
nicolasbalduino <nicolasbalduino@outlook.com>
2023-03-03 15:09:19 -0300

    Add project files.

---
+++ Calculadora/Program.cs
@@ -0,0 +1,16 @@
+﻿float n1, n2;
+
+Console.WriteLine("Dgite o primeiro numero: ");
+n1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Dgite o primeiro numero: ");
+n2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("A soma é igual a " + (n1 + n2));
+Console.WriteLine("A subtracao é igual a " + (n1 - n2));
+Console.WriteLine("A multiplicacao é igual a " + (n1 * n2));
+
+if (n2 == 0)
+    Console.WriteLine("Impossivel dividir por ZERO!");
+else
+    Console.WriteLine("A dvisao é igual a " + (n1 / n2));
